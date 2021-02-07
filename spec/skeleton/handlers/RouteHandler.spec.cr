3b7d9ef874e1a25dd9085e280733749069d34c2a
Oblivious-Oblivious <me262thanos@gmail.com>
2021-02-07 03:49:40 +0200

    Wrote tests for current code.

---
+++ spec/skeleton/handlers/RouteHandler.spec.cr
@@ -0,0 +1,26 @@
+describe Skeleton::RouteHandler do
+    app = Skeleton::RouteHandler.new;
+    app.get "/" { |context| context; };
+    app.get "/path1/" { |context| context; };
+    app.get "/path2/subpath" { |context| context; };
+    app.get "/path3/:var1" { |context| context; };
+    routes = app.static_routes;
+    tree = app.tree;
+
+    it "defines static paths without variables" do
+        routes.keys.should contain "/GET";
+        routes.keys.should contain "/GET/";
+        routes.keys.should contain "/GET/path1";
+        routes.keys.should contain "/GET/path1/";
+        routes.keys.should contain "/GET/path2/subpath";
+        routes.keys.should contain "/GET/path2/subpath/";
+    end
+
+    it "does not define variables" do
+        routes.keys.should_not contain "/GET/path3/:var1/";
+        routes.keys.should_not contain "/GET/path3/:var1";
+    end
+
+    # it "saves variables in the tree" do
+    # end
+end
