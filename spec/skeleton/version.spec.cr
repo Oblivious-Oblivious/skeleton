e6692c30eb2764d0574eb2418f0ef5dc307ff397
Oblivious-Oblivious <me262thanos@gmail.com>
2021-02-07 04:26:19 +0200

    Update version.spec.cr

--- spec/skeleton/version.spec.cr
+++ spec/skeleton/version.spec.cr
@@ -1,5 +1,5 @@
 describe Skeleton do    
-    it "has a version of 0.1.0" do
-        Skeleton::VERSION.should eq "0.1.0";
+    it "has a version of 0.2.0" do
+        Skeleton::VERSION.should eq "0.2.0";
     end
 end
