879767062c78d1c1e16d76f2a19a76ec9d0ee73d
Oblivious-Oblivious <me262thanos@gmail.com>
2021-02-15 02:16:00 +0200

    Added a basic Authenticator handler (No encryption).

--- spec/skeleton/handlers/AuthenticationHandler.spec.cr
+++ spec/skeleton/handlers/AuthenticationHandler.spec.cr
@@ -1,6 +1,57 @@
+def get_io_context(handler, request)
+    io = IO::Memory.new;
+    response = HTTP::Server::Response.new io;
+    context = HTTP::Server::Context.new request, response;
+    handler.call context;
+    response.close;
+    io.rewind;
+
+    { io, context };
+end
+
+describe Skeleton::Authenticator do
+    it "authorizes the credentials" do
+        auth = Skeleton::Authenticator.new "oblivious", "42password";
+        auth.authorized?("oblivious", "42password").should eq true;
+        auth.authorized?("oblivious", "wrong").should eq false;
+        auth.authorized?("non existent username", "whatever").should eq false;
+        auth.authorized?("non existent username", "42password").should eq false;
+        auth.authorized?("", "something").should eq false;
+        auth.authorized?("something", "").should eq false;
+        auth.authorized?("", "").should eq false;
+    end
+end
+
 describe Skeleton::AuthenticationHandler do
     it "is an HTTP handler" do
-        auth_handler = Skeleton::AuthenticationHandler.new;
+        auth_handler = Skeleton::AuthenticationHandler.new("", "");
         auth_handler.responds_to?(:call).should eq true;
+    end
+
+    it "authorizes with correct credentials" do
+        auth = Skeleton::AuthenticationHandler.new "oblivious", "42password";
+        request = HTTP::Request.new "GET", "/",
+            headers: HTTP::Headers {"Authorization" => "Basic b2JsaXZpb3VzOjQycGFzc3dvcmQ="};
+        io, context = get_io_context auth, request;
+        response = HTTP::Client::Response.from_io io, decompress: false;
+        response.status_code.should eq 404;
+    end
+
+    it "returns a 401 error on wrong credentials" do
+        auth = Skeleton::AuthenticationHandler.new "oblivious", "false pass";
+        request = HTTP::Request.new "GET", "/",
+            headers: HTTP::Headers {"Authorization" => "Basic b2JsaXZpb3VzOjQycGFzc3dvcmQ="};
+        io, context = get_io_context auth, request;
+        response = HTTP::Client::Response.from_io io, decompress: false;
+        response.status_code.should eq 401;
+    end
+
+    it "returns a 401 error on without authentication" do
+        auth = Skeleton::AuthenticationHandler.new "oblivious", "42password";
+        request = HTTP::Request.new "GET", "/",
+            headers: HTTP::Headers {"Authorization" => "random stuff"};
+        io, context = get_io_context auth, request;
+        response = HTTP::Client::Response.from_io io, decompress: false;
+        response.status_code.should eq 401;
     end
 end
--- src/skeleton/handlers/AuthenticationHandler.cr
+++ src/skeleton/handlers/AuthenticationHandler.cr
@@ -1,7 +1,69 @@
+class Skeleton::Authenticator
+    getter :user, :pass;
+
+    def initialize(
+        @user : String?,
+        @pass : String?
+    )
+    end
+
+    def authorized?(username, password)
+        user == username && pass == password;
+    end
+end
+
 class Skeleton::AuthenticationHandler
     include HTTP::Handler;
 
+    BASIC = "Basic";
+    AUTH = "Authorization";
+    LOGIN_REQ = "Basic realm=\"Login Required\"";
+
+    property authenticator : Skeleton::Authenticator;
+
+    private def authorized?(value)
+        username, password = Base64.decode_string(value[BASIC.size + 1..-1]).split(':');
+        authenticator.authorized? username, password;
+    end
+
+    private def fail_to_authenticate(context)
+        # We did not authenticate
+        headers = HTTP::Headers.new;
+        context.response.status_code = 401;
+        context.response.headers["WWW-Authenticate"] = LOGIN_REQ;
+        context.response.print "Could not authenticate";
+        "";
+    end
+
+    private def value_is_valid?(value)
+        # Check for value nullity here
+        value && value.size > 0 && value.starts_with? BASIC;
+    end
+
+    private def authorize_non_nil_value(context, value)
+        if authorized? value
+            call_next context;
+        else
+            fail_to_authenticate context;
+        end
+    end
+
+    private def perform_authentication(context, value)
+        if value_is_valid? value
+            authorize_non_nil_value context, value;
+        else
+            fail_to_authenticate context;
+        end
+    end
+
+    def initialize(
+        user : String?,
+        pass : String?
+    )
+        @authenticator = Skeleton::Authenticator.new user, pass;
+    end
+
     def call(context : HTTP::Server::Context)
-        call_next context;
+        perform_authentication context, context.request.headers[AUTH];
     end
 end
